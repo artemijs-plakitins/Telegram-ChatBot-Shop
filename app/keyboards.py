50e8a570767bfa2a445637ba39e200084a562889
artemijs-plakitins <plakitins@gmail.com>
2025-03-26 22:01:58 +0200

    26.03.2025

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,13 +1,13 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                             InlineKeyboardMarkup, InlineKeyboardButton)
 
-# open shop (reply)
+
 workerMenuKeys = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Deliver today')],
     [KeyboardButton(text='Already delivered')],
     [KeyboardButton(text='Work-Account')]],resize_keyboard=True, input_field_placeholder='Choose a button...')
 
-# open catalog (inline)
+
 cityKeys = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Riga',callback_data='rigaCity')],
     [InlineKeyboardButton(text='Liepaja',callback_data='liepajaCity')],
