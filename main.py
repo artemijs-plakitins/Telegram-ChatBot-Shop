d92b4c393a5c4c81fae7239c2d2b53e2f856bec5
artemijs-plakitins <plakitins@gmail.com>
2025-03-29 12:03:02 +0200

    29.03.2025

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ async def main() -> None:
     await async_main()
     await insert_data()
     load_dotenv()
-    bot = Bot(token=os.getenv('TOKEN'))
+    bot = Bot(token = os.getenv('TOKEN'))
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     asyncio.run(main())
